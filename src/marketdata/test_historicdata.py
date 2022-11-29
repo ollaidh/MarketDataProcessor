4731496914c5c2c3489d3879be54a6a1da77bfc3
Maria Lineva <ollaidh@programmer.net>
2022-11-29 14:33:55 +0200

    date_from_years fucntion now generates list of datetime objects from list of int

--- src/marketdata/test_historicdata.py
+++ src/marketdata/test_historicdata.py
@@ -1,7 +1,7 @@
 import unittest
 
 from marketdata import historicdata
-from marketdata.utils import date_from_year
+from marketdata.utils import date_from_years
 import numpy as np
 
 
@@ -9,11 +9,22 @@ class TestHistoricData(unittest.TestCase):
     def test_create(self):
         hd1 = [
             historicdata.HistoricData(
-                "IBM", [date_from_year(2002), date_from_year(2003), date_from_year(2004)], np.array([10, 20, 30])),
+                "IBM", date_from_years([2002, 2003, 2004]), np.array([10, 20, 30])),
             historicdata.HistoricData(
-                "TSLA", [date_from_year(2002), date_from_year(2003), date_from_year(2004)], np.array([11, 21, 31])),
+                "TSLA", date_from_years([2002, 2003, 2004]), np.array([11, 21, 31])),
             historicdata.HistoricData(
-                "APPL", [date_from_year(2002), date_from_year(2003), date_from_year(2004)], np.array([10, 20, 30]))
+                "APPL", date_from_years([2002, 2003, 2004]), np.array([10, 20, 30]))
+        ]
+        hd2 = [
+            historicdata.HistoricData(
+                "NTFLX", date_from_years([2012, 2013, 2014, 2015]),
+                np.array([110, 120, 130, 140])),
+            historicdata.HistoricData(
+                "FB", date_from_years([2012, 2013, 2014, 2015]),
+                np.array([111, 121, 131, 141])),
+            historicdata.HistoricData(
+                "INST", date_from_years([2012, 2013, 2014, 2015]),
+                np.array([112, 122, 132, 142]))
         ]
 
         # test hd1 case:
@@ -26,6 +37,20 @@ class TestHistoricData(unittest.TestCase):
         self.assertEqual(len(hd1[2].dates), 3)
         self.assertTrue((hd1[0].price == np.array([10, 20, 30])).all())
         self.assertTrue((hd1[1].price == np.array([11, 21, 31])).all())
+
+        # test hd2 case:
+        self.assertEqual(len(hd2), 3)
+        self.assertEqual(hd2[0].company, "NTFLX")
+        self.assertEqual(hd2[1].company, "FB")
+        self.assertEqual(hd2[2].company, "INST")
+        self.assertEqual(len(hd2[0].dates), 4)
+        self.assertEqual(len(hd2[1].dates), 4)
+        self.assertEqual(len(hd2[2].dates), 4)
+        self.assertTrue((hd2[0].price == np.array([110, 120, 130, 140])).all())
+        self.assertTrue((hd2[1].price == np.array([111, 121, 131, 141])).all())
+
+        # test calc_average_price function:
+
 
 
 if __name__ == '__main__':
--- src/marketdata/utils.py
+++ src/marketdata/utils.py
@@ -1,5 +1,8 @@
 import datetime
 
 
-def date_from_year(year: int) -> datetime.datetime:  # create timestamp date from year only
-    return datetime.datetime(year=year, month=1, day=1, hour=1, minute=1, second=1)
+def date_from_years(years: list[int]) -> list[datetime.datetime]:  # create timestamp date from year only
+    result = []
+    for year in years:
+        result.append(datetime.datetime(year=year, month=1, day=1, hour=1, minute=1, second=1))
+    return result
