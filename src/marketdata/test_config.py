710d57d30f7cce23e11b8c3e4bf1df6efe25a187
Maria Lineva <ollaidh@programmer.net>
2022-12-04 20:47:35 +0200

    Fix missing processor in test_config

--- src/marketdata/test_config.py
+++ src/marketdata/test_config.py
@@ -4,12 +4,19 @@ from marketdata import config
 
 class TestConfig(unittest.TestCase):
     def test_config(self):
-        inp1 = {"tickers": ["IBM", "TSLA"], "period": "1m", "interval": "1d", "price": ["open", "close"]}
+        inp1 = {
+            "tickers": ["IBM", "TSLA"],
+            "period": "1m",
+            "interval": "1d",
+            "price": ["open", "close"],
+            "processor": "minmax"
+        }
         conf = config.parse_config(inp1)
         self.assertEqual(conf.tickers, inp1["tickers"])
         self.assertEqual(conf.period, inp1["period"])
         self.assertEqual(conf.interval, inp1["interval"])
         self.assertEqual(conf.price, inp1["price"])
+        self.assertEqual(conf.processor, inp1["processor"])
 
 
 if __name__ == '__main__':
