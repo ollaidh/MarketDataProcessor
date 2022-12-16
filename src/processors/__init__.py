9023c2c110a2441fb7dfd70ba46fe04bfa2c7432
Maria Lineva <ollaidh@programmer.net>
2022-12-16 15:22:33 +0200

    Deleted processor_corrmatrix from __init__.py

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,5 +1,4 @@
 from .processor_minmax import *
 from .processor_percdelta import *
 from .processor_percdelta_minmax import *
-from .processor_corrmatrix import *
 from .processor_volatility import *
