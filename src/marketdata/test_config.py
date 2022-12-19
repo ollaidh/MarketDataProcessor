09798fbdb182bdf3b3e56239fcfabbe07efc6ccf
Maria Lineva <ollaidh@programmer.net>
2022-12-19 15:16:54 +0200

    Split tests for different cases according to Give-When-Then way

--- src/marketdata/test_config.py
+++ src/marketdata/test_config.py
@@ -3,7 +3,13 @@ from marketdata import config
 
 
 class TestConfig(unittest.TestCase):
-    def test_config(self):
+    def test_config_with_period(self):
+        # GIVEN
+        #   json with period for stock data presented as period
+        # WHEN
+        #   parsing config from json
+        # THEN
+        #   parameters after calling function should be equal to input, except "start_date" and "end_date" which should be None
         inp1 = {
             "tickers": ["IBM", "TSLA"],
             "period": "1m",
@@ -15,11 +21,18 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(conf.tickers, inp1["tickers"])
         self.assertEqual(conf.period, inp1["period"])
         self.assertEqual(conf.interval, inp1["interval"])
-        self.assertEqual(conf.start_date, None)
-        self.assertEqual(conf.end_date, None)
+        self.assertIsNone(conf.start_date)
+        self.assertIsNone(conf.end_date)
         self.assertEqual(conf.price, inp1["price"])
         self.assertEqual(conf.processor, inp1["processor"])
 
+    def test_config_with_period_format(self):
+        # GIVEN
+        #   json with period for stock data passed incorretly: period, start date and end date at the same time
+        # WHEN
+        #   parsing config from json
+        # THEN
+        #   parameters after calling function should be equal to input, except "period" which should be None
         inp2 = {
             "tickers": ["IBM", "TSLA"],
             "period": "1m",
@@ -31,6 +44,13 @@ class TestConfig(unittest.TestCase):
         }
         self.assertRaises(config.InvalidPeriodParameters, config.parse_config, inp2)
 
+    def test_config_with_startend(self):
+        # GIVEN
+        #   json with period for stock data presented as start date and end date
+        # WHEN
+        #   parsing config from json
+        # THEN
+        #   parameters after calling function should be equal to input, except "period" which should be None
         inp3 = {
             "tickers": ["IBM", "TSLA"],
             "start_date": "2021-12-12",
@@ -41,7 +61,7 @@ class TestConfig(unittest.TestCase):
         }
         conf = config.parse_config(inp3)
         self.assertEqual(conf.tickers, inp3["tickers"])
-        self.assertEqual(conf.period, None)
+        self.assertIsNone(conf.period)
         self.assertEqual(conf.interval, inp3["interval"])
         self.assertEqual(conf.start_date, inp3["start_date"])
         self.assertEqual(conf.end_date, inp3["end_date"])
